12cab313141e62c1283d58f047285e8362764a1d
Mikhail Leonov <mikecommon@gmail.com>
2026-09-18 17:07:17 -0400

    Auto backup 2026-09-18-17-07-15

---
+++ public/js/lng/en.js
@@ -0,0 +1,12 @@
+/* js/lng/en.js — English language pack. Registers itself with the i18n engine. */
+(function (FT) {
+  "use strict";
+  FT.I18n.register("en", {
+    name: "English",
+    nativeName: "English",
+    dir: "ltr",
+    strings: {
+
+    }
+  });
+})(window.FT);
---
+++ public/js/lng/ru.js
@@ -0,0 +1,12 @@
+/* js/lng/ru.js — Russian language pack (Русский). Registers itself with the i18n engine. */
+(function (FT) {
+  "use strict";
+  FT.I18n.register("ru", {
+    name: "Russian",
+    nativeName: "\u0420\u0443\u0441\u0441\u043a\u0438\u0439",
+    dir: "ltr",
+    strings: {
+
+    }
+  });
+})(window.FT);
